01c0b4d63cc9fa9dc28dff306a059db6e66669ef
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-08-25 11:35:57 +0200

    Component Featured was created and finished.

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,12 +3,16 @@ import {Navbar} from "../../components/Navbar/Navbar";
 import {Header} from "../../components/header/Header";
 
 import './Home.css';
+import {Featured} from "../../components/Featured/Featured";
 
 export const Home = () => {
     return (
         <div>
             <Navbar/>
             <Header type=""/>
+            <div className="homeContainer">
+                <Featured/>
+            </div>
         </div>
     );
 };
