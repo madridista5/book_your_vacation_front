e92411864bf6fa0dfc585eb8018a49c354225a1b
Tymoteusz Lossy <tymek.lossy@gmail.com>
2022-09-01 09:37:41 +0200

    UserData from localstorage was added.

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './App';
 import {SearchContext} from "./context/search.context";
-import { AuthContext } from './context/auth.context';
 import reportWebVitals from './reportWebVitals';
 
 import './index.css';
@@ -25,17 +24,7 @@ root.render(
             room: 0,
         },
     }}>
-        <AuthContext.Provider value={{
-            user: {
-                _id: '',
-                username: '',
-                email: '',
-            },
-            loading: false,
-            error: null,
-        }}>
         <App/>
-        </AuthContext.Provider>
     </SearchContext.Provider>
     // </React.StrictMode>
 );
